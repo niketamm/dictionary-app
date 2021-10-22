6085fd3667129bb7c67f1cdefc7ac7be66f7998e
Nike <reintamm.nike@gmail.com>
2021-10-23 00:35:16 +0900

    search function to get value

--- src/SearchWord.js
+++ src/SearchWord.js
@@ -1,15 +1,28 @@
-import React from "react";
 import "./SearchStyle.css";
+import { useState } from "react";
 
 export default function SearchWord(){
-     return <form>
-         <div className="searchEngine">
+let [word, setWord]= useState("");
+
+function search(event){
+    event.preventDefault();
+alert (`${word}`)
+;}
+
+function searchInput(event) {
+    setWord(event.target.value)
+    ;}
+
+return (
+<div className="searchEngine">
+<form onSubmit={search}>         
          <div className="input-group mb-3">
-  <input type="text" className="form-control" placeholder="Search"/>
+  <input type="search" className="form-control" placeholder="Search" onChange={searchInput}/>
   <div className="input-group-append">
-    <button className="btn btn-info" type="button">Button</button>
+    <button className="btn btn-info" type="submit">Button</button>
   </div>
 </div>
-</div>
+</form>
+</div>)
 
-     </form>;}
+     ;}
